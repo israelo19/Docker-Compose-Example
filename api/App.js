8c1b8ed4eddd2ff93c0fbeeba75e59785cc29126
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2024-07-02 16:02:36 -0400

    update

--- api/App.js
+++ api/App.js
@@ -11,9 +11,10 @@ let***REMOVED***port***REMOVED***=***REMOVED***process.env.PORT;
 App.use(cors());
 App.options('*',***REMOVED***cors());
 
-
-
+let***REMOVED***user***REMOVED***=***REMOVED***"d"
+let***REMOVED***pass***REMOVED***=***REMOVED***"dd"
 let***REMOVED***counter***REMOVED***=***REMOVED***0;
+let***REMOVED***loggedIn***REMOVED***=***REMOVED***false;
 
 //SET***REMOVED***UP***REMOVED***ROUTES
 App.get('/',***REMOVED***function(req,***REMOVED***res){
@@ -23,12 +24,22 @@ App.get('/',***REMOVED***function(req,***REMOVED***res){
 ***REMOVED***
 
 
+//***REMOVED***App.get('/count',***REMOVED***function(req,***REMOVED***res){
+//***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***counter++;
+//***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***//***REMOVED***res.end(`Count:***REMOVED***${counter}`)
+//***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***res.json({count:***REMOVED***counter})
+//***REMOVED***});
+App.get('/login',***REMOVED***function(req,***REMOVED***res){
+***REMOVED******REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED***loggedIn***REMOVED***=***REMOVED***!loggedIn;
+***REMOVED******REMOVED******REMOVED******REMOVED***//***REMOVED***res.end(`Logged***REMOVED***In:***REMOVED***${logged}`)
+***REMOVED******REMOVED******REMOVED******REMOVED***//***REMOVED***console.log(`Logged***REMOVED***In:***REMOVED***${logged}`)
+***REMOVED******REMOVED******REMOVED******REMOVED***console.log("Logged:",***REMOVED***loggedIn);
 
-App.get('/count',***REMOVED***function(req,***REMOVED***res){
-***REMOVED******REMOVED******REMOVED******REMOVED***counter++;
-***REMOVED******REMOVED******REMOVED******REMOVED***//***REMOVED***res.end(`Count:***REMOVED***${counter}`)
-***REMOVED******REMOVED******REMOVED******REMOVED***res.json({count:***REMOVED***counter})
+***REMOVED******REMOVED******REMOVED******REMOVED***res.json({loggedIn})
+***REMOVED******REMOVED******REMOVED******REMOVED***//***REMOVED***res.json({log:***REMOVED***logged})
 });
+
 App.delete('/count',***REMOVED***function(req,***REMOVED***res){
 ***REMOVED******REMOVED******REMOVED******REMOVED***counter***REMOVED***=***REMOVED***0;
 ***REMOVED******REMOVED******REMOVED******REMOVED***//***REMOVED***res.end(`Count:***REMOVED***${counter}`)
@@ -39,6 +50,13 @@ App.delete('/count',***REMOVED***function(req,***REMOVED***res){
 
 App.get('/sum/:v1/:v2',***REMOVED***(req,***REMOVED***res)***REMOVED***=>***REMOVED***{
 ***REMOVED******REMOVED******REMOVED******REMOVED***const***REMOVED***{v1,***REMOVED***v2}***REMOVED***=***REMOVED***req.params;
+***REMOVED******REMOVED******REMOVED******REMOVED***res.json({sum:((+v1)+(+v2))})
+});
+
+App.post('/login',***REMOVED***(req,***REMOVED***res)***REMOVED***=>***REMOVED***{
+***REMOVED******REMOVED******REMOVED******REMOVED***const***REMOVED***{username,***REMOVED***password}***REMOVED***=***REMOVED***req.params;
+
+***REMOVED******REMOVED******REMOVED******REMOVED***//logic***REMOVED***check***REMOVED***with***REMOVED***username***REMOVED***and***REMOVED***pass
 ***REMOVED******REMOVED******REMOVED******REMOVED***res.json({sum:((+v1)+(+v2))})
 });
 
--- www/src/App.js
+++ www/src/App.js
@@ -1,5 +1,5 @@
 import***REMOVED***axios***REMOVED***from***REMOVED***'axios';
-import***REMOVED***React,***REMOVED***{useState}***REMOVED***from***REMOVED***'react';
+import***REMOVED***React,***REMOVED***{***REMOVED***useState***REMOVED***}***REMOVED***from***REMOVED***'react';
 import***REMOVED***logo***REMOVED***from***REMOVED***'./logo.svg';
 import***REMOVED***'./App.css';
 
@@ -7,15 +7,17 @@ import***REMOVED***'./App.css';
 
 
 const***REMOVED***App***REMOVED***=***REMOVED***()***REMOVED***=>***REMOVED***{
+***REMOVED******REMOVED***const***REMOVED***[logs,***REMOVED***setLog]***REMOVED***=***REMOVED***useState(false)
+***REMOVED******REMOVED***
 
 ***REMOVED******REMOVED***const***REMOVED***[count,***REMOVED***setCount]***REMOVED***=***REMOVED***useState(0)
 ***REMOVED******REMOVED***const***REMOVED***updateCounter***REMOVED***=***REMOVED***()***REMOVED***=>***REMOVED***{
 ***REMOVED******REMOVED******REMOVED******REMOVED***axios.get('/api/count').then((result)***REMOVED***=>***REMOVED***{
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***if(result.data***REMOVED***&&***REMOVED***result.data.count){
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***if***REMOVED***(result.data***REMOVED***&&***REMOVED***result.data.count)***REMOVED***{
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***setCount(result.data.count)
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***}
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***//console.log({***REMOVED***details:***REMOVED***result***REMOVED***});
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log({count});
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log({***REMOVED***count***REMOVED***});
 ***REMOVED******REMOVED******REMOVED******REMOVED***}).catch((e)***REMOVED***=>***REMOVED***{
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log('Error***REMOVED***Connecting',***REMOVED***e)
 ***REMOVED******REMOVED******REMOVED******REMOVED***});
@@ -24,16 +26,34 @@ const***REMOVED***App***REMOVED***=***REMOVED***()***REMOVED***=>***REMOVED***{
 
 ***REMOVED******REMOVED***const***REMOVED***reset***REMOVED***=***REMOVED***()***REMOVED***=>***REMOVED***{
 ***REMOVED******REMOVED******REMOVED******REMOVED***axios.delete('/api/count').then((result)***REMOVED***=>***REMOVED***{
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***if(result.data***REMOVED***&&***REMOVED***!Number.isNaN(+result.data.count)){
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***if***REMOVED***(result.data***REMOVED***&&***REMOVED***!Number.isNaN(+result.data.count))***REMOVED***{
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***setCount(result.data.count)
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***}
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***//console.log({***REMOVED***details:***REMOVED***result***REMOVED***});
-***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log({count});
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log({***REMOVED***count***REMOVED***});
 ***REMOVED******REMOVED******REMOVED******REMOVED***}).catch((e)***REMOVED***=>***REMOVED***{
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log('Error***REMOVED***Connecting',***REMOVED***e)
 ***REMOVED******REMOVED******REMOVED******REMOVED***});
 ***REMOVED******REMOVED***}
 
+***REMOVED******REMOVED***console.log({logs});
+
+***REMOVED******REMOVED***const***REMOVED***login***REMOVED***=***REMOVED***()***REMOVED***=>***REMOVED***{
+***REMOVED******REMOVED******REMOVED******REMOVED***axios.get('/api/login').then((result)***REMOVED***=>***REMOVED***{
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log({logs});
+
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***//***REMOVED***if***REMOVED***(result***REMOVED***===***REMOVED***false)***REMOVED***{
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***//***REMOVED***logs.data***REMOVED***=***REMOVED***true;
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***setLog(result.data.loggedIn);
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***//***REMOVED***}
+
+
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log({logs});
+***REMOVED******REMOVED******REMOVED******REMOVED***}).catch((e)***REMOVED***=>***REMOVED***{
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***console.log('Error***REMOVED***Connecting',***REMOVED***e)
+***REMOVED******REMOVED******REMOVED******REMOVED***});
+
+***REMOVED******REMOVED***}
 ***REMOVED******REMOVED***return***REMOVED***(
 ***REMOVED******REMOVED******REMOVED******REMOVED***<div***REMOVED***className="App">
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<header***REMOVED***className="App-header">
@@ -44,6 +64,17 @@ const***REMOVED***App***REMOVED***=***REMOVED***()***REMOVED***=>***REMOVED***{
 
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<button***REMOVED***onClick={updateCounter}>{`Current***REMOVED***Count***REMOVED***${count}`}</button>
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<button***REMOVED***onClick={reset}>Reset</button>
+
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<div***REMOVED***className="textBox">
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<input***REMOVED***type="text"***REMOVED***placeholder="Enter***REMOVED***Username"***REMOVED***name='user'></input>
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***</div>
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<div***REMOVED***className="textBox">
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<input***REMOVED***type="text"***REMOVED***placeholder="Enter***REMOVED***Password"***REMOVED***name='pass'></input>
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***</div>
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<button***REMOVED***className="loginBtn"***REMOVED***onClick={login}>Login</button>
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***<h2>{`***REMOVED***Logged***REMOVED***In:***REMOVED***${logs}`}</h2>
 ***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***</header>
 ***REMOVED******REMOVED******REMOVED******REMOVED***</div>
 ***REMOVED******REMOVED***);

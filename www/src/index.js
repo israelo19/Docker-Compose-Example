f53e5f50d344c3de51c0c9b1497235b0644737db
sspadhayes <dhayes@sspa.com>
2024-07-02 08:06:19 -0400

    roll back to react 17 to get hot relaoding working....this is lazy, but I didnt want to learn how to do it the right way in react 18 :)

--- www/src/index.js
+++ www/src/index.js
@@ -1,17 +1,18 @@
-import***REMOVED***React***REMOVED***from***REMOVED***'react';
-import***REMOVED***ReactDOM***REMOVED***from***REMOVED***'react-dom/client';
+
+import***REMOVED***React***REMOVED***from***REMOVED***"react";
+import***REMOVED***ReactDOM***REMOVED***from***REMOVED***"react-dom";
+
+import***REMOVED*******REMOVED***as***REMOVED***serviceWorker***REMOVED***from***REMOVED***"./serviceWorker";
+
 import***REMOVED***'./index.css';
 import***REMOVED***App***REMOVED***from***REMOVED***'./App';
-import***REMOVED***reportWebVitals***REMOVED***from***REMOVED***'./reportWebVitals';
 
-const***REMOVED***root***REMOVED***=***REMOVED***ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-***REMOVED******REMOVED***<React.StrictMode>
+ReactDOM.render(
+***REMOVED******REMOVED***//***REMOVED***<Provider***REMOVED***store={store}>***REMOVED***//***REMOVED***this***REMOVED***is***REMOVED***for***REMOVED***later***REMOVED***(Redux)
 ***REMOVED******REMOVED******REMOVED******REMOVED***<App***REMOVED***/>
-***REMOVED******REMOVED***</React.StrictMode>
+***REMOVED******REMOVED***//***REMOVED***</Provider>
+***REMOVED******REMOVED***,
+***REMOVED******REMOVED***document.getElementById("root")
 );
 
-//***REMOVED***If***REMOVED***you***REMOVED***want***REMOVED***to***REMOVED***start***REMOVED***measuring***REMOVED***performance***REMOVED***in***REMOVED***your***REMOVED***app,***REMOVED***pass***REMOVED***a***REMOVED***function
-//***REMOVED***to***REMOVED***log***REMOVED***results***REMOVED***(for***REMOVED***example:***REMOVED***reportWebVitals(console.log))
-//***REMOVED***or***REMOVED***send***REMOVED***to***REMOVED***an***REMOVED***analytics***REMOVED***endpoint.***REMOVED***Learn***REMOVED***more:***REMOVED***https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
